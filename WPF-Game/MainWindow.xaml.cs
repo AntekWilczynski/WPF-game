f29d8b39bc3e3a32bc1a4a947211a6233f9255c2
Antek Wilczyński <a.s.wilczynski@gmail.com>
2020-08-21 11:02:48 +0200

    Adding first variables and sprites

--- WPF-Game/MainWindow.xaml.cs
+++ WPF-Game/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPF_Game
 {
@@ -20,9 +21,58 @@ namespace WPF_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        // create a new instance of the dispatcher timer class called gametimer
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        // create new Rect class instance called player hit box and enemy hit box
+        Rect playerHitBox;
+        Rect enemyHitBox;
+
+        // game over boolean
+        bool gameover = false;
+        // make a sprite int double variable, this will be used to swap the sprites for player
+        double spriteInt = 0;
+
+        // make three image brush instances called player sprite, background sprite and enemy sprite
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgroundSprite = new ImageBrush();
+        ImageBrush enemySprite = new ImageBrush();
+
+        //integer array which we will use to change the enemy position on screen
+        int[] enemyPosition = {1 ,2 ,3  };
+        // empty integer called score
+        int score = 0;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Canvas_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void StartGame()
+        {
+
+        }
+
+        private void runSprite(double i)
+        {
+
+        }
+
+        private void gameEngine(object sender, EventArgs e)
+        {
+
         }
     }
 }
