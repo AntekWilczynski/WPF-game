278b0d88f1dfa9d580191b9d6ffe9b0d0dfa74d7
Antek Wilczyński <a.s.wilczynski@gmail.com>
2020-08-27 23:53:52 +0200

    Better hero animation

--- WPF-Game/MainWindow.xaml.cs
+++ WPF-Game/MainWindow.xaml.cs
@@ -18,13 +18,9 @@ namespace WPF_Game
 {
     public partial class MainWindow : Window
     {
-
-
-        // create new Rect class instance called player hit box and enemy hit box
         Rect graczHitBox;
         Rect przeciwnikHitBox;
         Rect przeszkodaHitBox;
-
 
         DispatcherTimer gameTimer = new DispatcherTimer();
         Random rand = new Random();
@@ -49,7 +45,8 @@ namespace WPF_Game
 
             graczHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
             przeciwnikHitBox = new Rect(Canvas.GetLeft(monster_angel), Canvas.GetTop(monster_angel), monster_angel.Width, monster_angel.Height);
-            przeszkodaHitBox = new Rect(Canvas.GetLeft(przeszkoda), Canvas.GetTop(przeszkoda), przeszkoda.Width, przeszkoda.Height);
+           // przeszkodaHitBox = new Rect(Canvas.GetLeft(przeszkoda), Canvas.GetTop(przeszkoda), przeszkoda.Width, przeszkoda.Height);
+        
         }
         private void HandleEsc(object sender, KeyEventArgs e)
         {
@@ -58,40 +55,22 @@ namespace WPF_Game
         }
         private void Canvas_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Up && Canvas.GetTop(background) < 460)
+            if (e.Key == Key.Up && Canvas.GetTop(background) < 470)
             {
                 Canvas.SetTop(background, Canvas.GetTop(background) + 10);
                 Canvas.SetTop(monster_angel, Canvas.GetTop(monster_angel) + 10);
-                //graczHitBox.Location = new Point(Canvas.GetTop(monster_angel), Canvas.GetLeft(monster_angel));
-                //przeciwnikHitBox.Location = new Point(Canvas.GetTop(player), Canvas.GetLeft(player));
 
-                if (graczHitBox.IntersectsWith(przeciwnikHitBox))
-                {
-                    Window win2 = new Window();
-                    win2.Show();
-                }
             }
             else if (e.Key == Key.Down && Canvas.GetTop(background) + background.Height > 700)
             {
                 Canvas.SetTop(background, Canvas.GetTop(background) - 10);
                 Canvas.SetTop(monster_angel, Canvas.GetTop(monster_angel) - 10);
-                if (graczHitBox.IntersectsWith(przeciwnikHitBox))
-                {
-                    Window win2 = new Window();
-                    win2.Show();
-                }
             }
-
-            else if (e.Key == Key.Right && Canvas.GetLeft(background) + background.Width > 1000)
+            else if (e.Key == Key.Right && Canvas.GetLeft(background) + background.Width > 1060)
             {
                 Canvas.SetLeft(background, Canvas.GetLeft(background) - 10);
                 Canvas.SetLeft(monster_angel, Canvas.GetLeft(monster_angel) - 10);
-                if (graczHitBox.IntersectsWith(przeciwnikHitBox))
-                {
-                    Window win2 = new Window();
-                    win2.Show();
-                }
-
+               
             }
             else if (e.Key == Key.Left && Canvas.GetLeft(background) < 750)
             {
@@ -106,47 +85,55 @@ namespace WPF_Game
         }
         private void StartGame()
         {
-            runSprite(1);
-            graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro.png"));
+            graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero1.png"));
             przeciwnikSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/angel.png"));
             player.Fill = graczSprite;
             monster_angel.Fill = przeciwnikSprite;
             gameTimer.Start();
         }
-
-
         private void runSprite(double i)
         {
             switch (i)
             {
                 case 1:
-                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro3.png"));
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero1.png"));
                     break;
                 case 2:
-                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro2.png"));
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero2.png"));
                     break;
                 case 3:
-                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro1.png"));
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero3.png"));
                     break;
                 case 4:
-                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro3.png"));
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero4.png"));
                     break;
                 case 5:
-                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/H_nekro4.png"));
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero5.png"));
+                    break;
+                case 6:
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero6.png"));
+                    break;
+                case 7:
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero7.png"));
+                    break;
+                case 8:
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero8.png"));
+                    break;
+                case 9:
+                    graczSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/Hero9.png"));
                     break;
             }
             player.Fill = graczSprite;
         }
-
         private void gameEngine(object sender, EventArgs e)
         {
             graczHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
-            przeszkodaHitBox = new Rect(Canvas.GetLeft(przeszkoda), Canvas.GetTop(przeszkoda), przeszkoda.Width, przeszkoda.Height);
+            //  przeszkodaHitBox = new Rect(Canvas.GetLeft(przeszkoda), Canvas.GetTop(przeszkoda), przeszkoda.Width, przeszkoda.Height);
             playerAnimationSpriteCounter += .5;
             runSprite(playerAnimationSpriteCounter);
             if (playerAnimationSpriteCounter > 5)
                 playerAnimationSpriteCounter = 1;
-           
+
         }
     }
 }
